d66c3ffb05fb7f649d3bf9b6c982f349796c83c2
Christian-Hermann <christianghermann@gmail.com>
2025-01-21 18:39:49 -0600

    FizzBuzz

--- day-2/greeter-app.js
+++ day-2/greeter-app.js
@@ -35,6 +35,8 @@
 
 function greeter(hour) {
     // YOUR CODE BELOW HERE //
+ 
+    
     
     
     
--- day-2/homework/fizz-buzz.js
+++ day-2/homework/fizz-buzz.js
@@ -12,12 +12,21 @@
 */
 function fizzBuzz() {
     // YOUR CODE GOES BELOW HERE //
-
-
-
+// write a function that prints the numbers from 1 - 100
+ for (let i = 1; i <= 100; i++){
+      if (i % 3 === 0 & i % 5 === 0){
+        console.log("FizzBuzz");
+      } else if (i % 5 === 0){
+        console.log("Buzz");
+      } else if (i % 3 === 0){
+        console.log("Fizz");
+      } else {
+        console.log(i);
+      }
+      } 
+    
     // YOUR CODE GOES ABOVE HERE //
 }
-
 
 
 
